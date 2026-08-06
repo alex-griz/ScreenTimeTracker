7bb7f952262dee6f94b3b0d667fa0e2f9f300d05
Griz394 <grizzlymine394@gmail.com>
2026-08-06 20:31:22 +0400

    Run as Service
    Теперь приложение запускается как служба Windows. Это сделано для работы в фоновом режиме, без консоли

--- Program.cs
+++ Program.cs
@@ -1,44 +1,12 @@
-﻿using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.ServiceProcess;
+
 namespace ScreenTimeTracker;
 
 class Program
 {
-    [DllImport("user32.dll")]
-    private static extern IntPtr GetForegroundWindow();
-    [DllImport("user32.dll")]
-    private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
-    [DllImport("user32.dll")]
-    private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
-
-    private static string currentApp = "";
-    private static DateTime startTime = DateTime.Now;
+    
     static void Main()
     {
-        DataBase.CreateDB();
-        var timer = new System.Timers.Timer(1000);
-        timer.Elapsed += (s,e)=>GetActiveApp();
-        timer.Start();
-
-        Console.WriteLine("Launched");
-        Console.ReadKey();
-    }
-    private static void GetActiveApp()
-    {
-        IntPtr hwnd =  GetForegroundWindow();
-        if (hwnd == IntPtr.Zero){return;}
-
-        GetWindowThreadProcessId(hwnd, out uint pid);
-        Process process = Process.GetProcessById((int)pid);
-        if (currentApp != process.ProcessName)
-        {
-            TimeSpan workTime = DateTime.Now - startTime;
-            Commands.WriteTime(workTime, currentApp);
-
-            currentApp = process.ProcessName;
-            startTime = DateTime.Now;
-            Console.WriteLine($"[DEBUG]Active window changed. New window:{currentApp}");
-        }
+        ServiceBase.Run(new BackgroundService());
     }
 }
